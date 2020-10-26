c033df70085686ab7b06afa6cb8c66136a812484
zeroclock <zeroclock@icloud.com>
2020-10-26 14:20:27 +0900

    Implemented base64 decoding

--- src/base64.rs
+++ src/base64.rs
@@ -1,4 +1,5 @@
 const BASE64: [char; 64] = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'];
+const BASE64_STR: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 pub fn base64_encode(input: &str) -> String {
     let input_bytes = input.as_bytes();
@@ -37,6 +38,36 @@ pub fn base64_encode(input: &str) -> String {
     result
 }
 
+pub fn base64_decode(input: &str) -> String {
+    let mut result = String::new();
+    let mut bits = String::new();
+    for c in input.chars() {
+        let base64_pos = BASE64_STR.find(c);
+        if let Some(pos) = base64_pos {
+            bits = format!("{}{:0>6b}", bits, pos);
+        } else {
+            bits = format!("{}000000", bits);
+        }
+    }
+    let mut idx: usize = 0;
+    println!("len: {}", bits.len());
+    while idx < bits.len() {
+        println!("idx: {}", idx);
+        let utf8: &[u8] = &[
+            u8::from_str_radix(
+                &bits[idx..idx.saturating_add(8)],
+                2,
+            ).unwrap()
+        ];
+        if utf8[0] != 0 {
+            let s = std::str::from_utf8(&utf8).unwrap();
+            result = format!("{}{}", result, s);
+        }
+        idx = idx.saturating_add(8);
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -59,6 +90,27 @@ mod tests {
     fn test_can_base64_encode_input_3() {
         let actual = base64_encode("!base64_encoding_is_awesome?");
         let expected = "IWJhc2U2NF9lbmNvZGluZ19pc19hd2Vzb21lPw==".to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_can_base64_decode_input_1() {
+        let actual = base64_decode("dGhpc2lzdGVzdGlucHV0");
+        let expected = "thisistestinput".to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_can_base64_decode_input_2() {
+        let actual = base64_decode("dXNlcm5hbWU6cGFzc3dvcmQ=");
+        let expected = "username:password".to_string();
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_can_base64_decode_input_3() {
+        let actual = base64_decode("IWJhc2U2NF9lbmNvZGluZ19pc19hd2Vzb21lPw==");
+        let expected = "!base64_encoding_is_awesome?".to_string();
         assert_eq!(expected, actual);
     }
 }
